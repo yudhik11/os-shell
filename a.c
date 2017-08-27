91ea0e698171e58fd60fe010184e7d466748fa10
yudhik <yudhik100@gmail.com>
2017-08-27 22:57:20 +0530

    normal echo

---
+++ a.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    char a[]="efbvesgdbvgdb\0";
+    printf("%s\n",a+3);
+    return 0;
+}
--- os.c
+++ os.c
@@ -5,15 +5,33 @@
 #include<netdb.h>
 #include<sys/types.h>
 #include<pwd.h>
+#include<string.h>
 int main (){
     char hostname[1024];
-    char cwd[1024];
     hostname[1023] = '\0';
+    char cwd[1024];
+    cwd[1023] = '\0';
     gethostname(hostname, 1023);//sys_name
     struct passwd *hello;
     hello=getpwuid(getuid());//user_name
     getcwd(cwd,sizeof (cwd));
+    int cwdlen=strlen(cwd);
+    int k=2;
+    for (int i=0;i<k;i++){
+        char pwd[1024];
+        pwd[1023] = '\0';
+        getcwd(pwd,sizeof (pwd));
+        int pwdlen=strlen(pwd);
+        if (pwdlen>=cwdlen)
+            printf("<%s@%s:~%s>$ ",hello->pw_name,hostname,pwd+cwdlen);
+        else
+            printf("<%s@%s:%s>$ ",hello->pw_name,hostname,cwd+pwdlen);
+//        char inp[10005]={'\0'};
+//        getchar();
+//        scanf("%[^\n]s",inp);
+//        printf("%s\n",inp);
 
-    printf("<%s@%s:%s>\n",hello->pw_name,hostname,cwd);
+    }
+
     return 0;
 }
